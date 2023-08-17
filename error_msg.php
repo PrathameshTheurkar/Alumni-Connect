20f3b5911ad97e35b36e8a294ab01fb038b37aae
Prathamesh <prathameshtheurkar037@gmail.com>
2023-08-17 07:21:17 +0530

    initial commit

---
+++ error_msg.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Error Message</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+    <h2>Name should contain only Alphabets !!</h2>
+</body>
+</html>
+
